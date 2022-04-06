67b1200392649180fb8c68a3412d86f723d29aca
inventor500 <isaacganoung@gmail.com>
2022-04-06 14:06:06 -0500

    Added prompt before leaving page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <v-app>
-    <app-header></app-header>
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+<v-app>
+  <app-header></app-header>
+  <v-main>
+    <router-view></router-view>
+  </v-main>
+</v-app>
 </template>
 
 <script>
 import MathJax from "@/assets/tex-svg.js"; // eslint-disable-line
 import AppHeader from "@/components/AppHeader.vue";
 export default {
-  name: "App",
-  components: {
-    AppHeader,
-  },
+    name: "App",
+    components: {
+        AppHeader,
+    },
+    beforeMount () {
+        window.addEventListener('beforeunload', function(event) {
+            console.log("User tried to leave the page."); // Replace with something else
+            event.returnValue="Are you sure you want to leave the test?"; // Older browsers apparently show this on prompt
+            event.preventDefault();
+        });
+    }
 };
+
+
+
 </script>
 
 <style>
