ac635323e42ff768a885babc7469dc952e126a1b
inventor500 <isaacganoung@gmail.com>
2022-05-04 08:08:30 -0500

    Fixed bug

--- src/components/StudentPopup.vue
+++ src/components/StudentPopup.vue
@@ -18,7 +18,7 @@
       </v-tooltip>
     </v-app-bar>
   </div>
-  <div id="content" v-click-outside="closePopup">
+  <div id="content">
     <div id="data_table">
       <v-card light>
         <v-card-title>
